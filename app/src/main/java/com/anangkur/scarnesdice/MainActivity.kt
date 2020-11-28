d6b160b7afeb5bd4e64dd73d75c369065c99967f
anangkur <anangk97@gmail.com>
2020-11-28 11:15:55 +0700

    feat(app): create method to generate random number from 1 to 6 and update iv_dice based on the generated number

--- app/src/main/java/com/anangkur/scarnesdice/MainActivity.kt
+++ app/src/main/java/com/anangkur/scarnesdice/MainActivity.kt
@@ -2,10 +2,45 @@ package com.anangkur.scarnesdice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.anangkur.scarnesdice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var userOverallScore = 0
+    private var userTurnScore = 0
+    private var comOverallScore = 0
+    private var comTurnScore = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnRoll.setOnClickListener { onRollClick() }
+    }
+
+    private fun onRollClick() {
+        val randomNumber = (1..6).random()
+        updateIvDice(randomNumber)
+    }
+
+    private fun updateIvDice(diceNumber: Int) {
+        binding.ivDice.setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                when (diceNumber) {
+                    1 -> R.drawable.dice1
+                    2 -> R.drawable.dice2
+                    3 -> R.drawable.dice3
+                    4 -> R.drawable.dice4
+                    5 -> R.drawable.dice5
+                    6 -> R.drawable.dice6
+                    else -> 0
+                }
+            )
+        )
     }
 }
